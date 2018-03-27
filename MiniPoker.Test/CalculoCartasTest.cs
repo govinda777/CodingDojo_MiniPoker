18bbb64d9be73aaa5bd43c1b8c4132cd8e2c4c6d
Luã Govinda Mendes Souza <lua.souza@iteris.com.br>
2018-03-27 11:42:38 -0300

    Commit inicial

---
+++ MiniPoker.Test/CalculoCartasTest.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace MiniPoker.Test
+{
+    [TestClass]
+    public class CalculoCartasTest
+    {
+        [TestMethod]
+        public void CalcularSequencia_CalcularSeHeSequencia_Test()
+        {
+            var cartas = new int[5] { 1 , 3 , 2 , 4 , 5};
+
+            bool isSequencia = new CalcularCarta(cartas).CalcularSequencia();
+
+            Assert.IsTrue(isSequencia);
+
+
+        }
+
+        [TestMethod]
+        public void CalcularSequencia_CalcularSeNHeSequencia_Test()
+        {
+            var cartas = new int[5] { 1, 3, 1, 4, 5 };
+
+            bool isSequencia = new CalcularCarta(cartas).CalcularSequencia();
+
+            Assert.IsFalse(isSequencia);
+        }
+
+
+        [TestMethod]
+        public void CalcularSequencia_CalcularSeNullSequencia_Test()
+        {
+            int[] cartas = null;
+            try
+            {
+                bool isSequencia = new CalcularCarta(cartas).CalcularSequencia();
+            }
+            catch (ArgumentNullException e) {
+                Assert.IsInstanceOfType(e, typeof(ArgumentNullException));
+                return;
+            }
+
+            Assert.Fail();
+        }
+
+    }
+}
---
+++ MiniPoker/CalcularCarta.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Linq;
+namespace MiniPoker
+{
+    public class CalcularCarta
+    {
+        private int[] _cartas;
+        public CalcularCarta(int[] cartas)
+        {
+            if(cartas == null)
+            {
+                throw new ArgumentNullException();
+            }
+            _cartas = Ordenar(cartas);
+        }
+
+        public bool CalcularSequencia() {
+            
+            for (int i = 0; i < _cartas.Length - 1; i++) {
+                if (_cartas[i]+1 != _cartas[i + 1]) {
+                    return false;
+                } 
+            }
+            return true;
+        }
+
+        public bool CalcularQuadra()
+        {
+            int[] newCartas = _cartas.Distinct().ToArray();
+
+            if(newCartas.Length==2) //dupla
+
+            if (newCartas.Length == 3) //trinca
+                
+            if (newCartas.Length == 4) //quadra
+
+        }
+
+        public int[] Ordenar(int[] cartas)
+        {
+            return cartas.OrderBy(x=> x).ToArray();
+        }
+
+    }
+}
